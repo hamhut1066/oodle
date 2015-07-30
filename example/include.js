89265e9565da1d6bd4d06fe6b82e32cd0406f620
Hamish Hutchings <hamhut1066@gmail.com>
2015-07-30 21:02:13 +0100

    Write tsconfig.json to build project.
    Include Example files in example subdirectory

---
+++ example/include.js
@@ -0,0 +1,44 @@
+/// <reference path="calc.ts" />
+/// <reference path="../typings/angularjs/angular.d.ts" />
+angular.module("fma2eng", [])
+  .controller("fma2angcontroller", ['$scope', 'calcService', function ($scope, calcService) {
+
+    $scope.calc = calcService.calc
+
+
+    $scope.do_it = function () {
+      $scope.calc.run().then(function () {
+        $scope.$apply();
+      })
+        .catch(function(data) {
+          console.log(data.stack)
+        })
+    };
+
+  }]);
+
+
+angular.module('fma2eng').service('calcService', function() {
+  // Data Service
+  var calc = new oodle.Calc({
+
+    M: new oodle.EngQ(12, "kg", {
+      range: {
+        min:{
+          inclusive: false,
+          val: 0
+        }},
+      name: "The mass of the bullet that will penetrate your skull"
+    }),
+
+    A: new oodle.EngQ(3, "m/s^2")
+
+  }, function (calc) {
+    return new oodle.EngQ(calc.A.val * calc.M.val, "N");
+  })
+
+
+  return {
+    calc: calc
+  }
+})
