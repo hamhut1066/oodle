e7ba34e4d49f6b33ae75a1aba91f12f562fd0e59
Hamish Hutchings <hamhut1066@gmail.com>
2015-08-13 09:06:43 +0100

    Add Install scripts for Vagrant.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y nginx
+apt-get install -y nginx npm git
 
+#Setting Env Variables
+SRC="/vagrant"
+PATH=./node_modules/.bin:$PATH
+
+# Setup of nginx.
+# If there are syncing issues, look into `sendfile`
 rm -rf /usr/share/nginx/html
 ln -s /vagrant /usr/share/nginx/html
+
+# Linking up node, as this is an issue with ubuntu
+ln -s /usr/bin/nodejs /usr/bin/node
+
+
+# Install npm dependencies
+cd $SRC
+npm install
+
+
+# Buld Project
+tsc
